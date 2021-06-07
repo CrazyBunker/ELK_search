20ae952399d620622b5bd68b9a96b3f391e9d363
Vladislav <vlad@krasnodar-it-service.ru>
2021-06-07 21:03:30 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, make_response
-import os
+import os,sys
 from elasticsearch import Elasticsearch
 
 
@@ -24,52 +24,52 @@ def get_url_params():
 @app.route('/data', methods=['GET'])
 def get_query_string():
     id_search, filename,index = get_url_params()
-   # try:
-    es = Elasticsearch([{'host': os.environ['ELK'], 'port': 9200}])
-    res = es.search(index=index, body={
-        "query": {
-            "terms": {
-                "_id": [id_search]
+    try:
+        es = Elasticsearch([{'host': os.environ['ELK'], 'port': 9200}])
+        res = es.search(index=index, body={
+            "query": {
+                "terms": {
+                    "_id": [id_search]
+                }
             }
-        }
-    })
-    for i in res['hits']['hits']:
-        source = i['_source']
-        delimeter = "#" * 50
-        outstring = ''
-        print(source)
-        if '@timestamp' in source:
-            outstring += '@timestamp:' + str(source['@timestamp']) + "\n" + delimeter + "\n"
-        if 'api' in source:
-            outstring += 'API:' + str(source['api']) + "\n" + delimeter + "\n"
+        })
+        for i in res['hits']['hits']:
+            source = i['_source']
+            delimeter = "#" * 50
+            outstring = ''
+            print(source)
+            if '@timestamp' in source:
+                outstring += '@timestamp:' + str(source['@timestamp']) + "\n" + delimeter + "\n"
+            if 'api' in source:
+                outstring += 'API:' + str(source['api']) + "\n" + delimeter + "\n"
 
-        if 'User' in source:
-            outstring += 'User:' + str(source['User']) + "\n" + delimeter + "\n"
-        if 'request' in source:
-            if 'url' in source['request']:
-                outstring += 'Request URL:' + str(source['request']['url']) + "\n" + delimeter + "\n"
-            if 'Host' in source['request']:
-                outstring += 'Host:' + str(source['Host']) + "\n" + delimeter + "\n"
-        if 'response' in source:
-            if 'body' in source['response']:
-                outstring += 'ResponseBody:' + str(source['response']['body']) + "\n" + delimeter + "\n"
-            if 'statusCode' in source['response']:
-                outstring += 'Response statusCode:' + str(source['response']['statusCode']) + "\n" + delimeter + "\n"
-        if 'RequestBody' in source:
-            outstring += 'RequestBody:' + str(source['RequestBody']) + "\n" + delimeter + "\n"
-        if 'ResponseBody' in source:
-            outstring += 'ResponseBody:' + str(source['ResponseBody']) + "\n" + delimeter + "\n"
+            if 'User' in source:
+                outstring += 'User:' + str(source['User']) + "\n" + delimeter + "\n"
+            if 'request' in source:
+                if 'url' in source['request']:
+                    outstring += 'Request URL:' + str(source['request']['url']) + "\n" + delimeter + "\n"
+                if 'Host' in source['request']:
+                    outstring += 'Host:' + str(source['Host']) + "\n" + delimeter + "\n"
+            if 'response' in source:
+                if 'body' in source['response']:
+                    outstring += 'ResponseBody:' + str(source['response']['body']) + "\n" + delimeter + "\n"
+                if 'statusCode' in source['response']:
+                    outstring += 'Response statusCode:' + str(source['response']['statusCode']) + "\n" + delimeter + "\n"
+            if 'RequestBody' in source:
+                outstring += 'RequestBody:' + str(source['RequestBody']) + "\n" + delimeter + "\n"
+            if 'ResponseBody' in source:
+                outstring += 'ResponseBody:' + str(source['ResponseBody']) + "\n" + delimeter + "\n"
 
-        if 'message' in source:
-            outstring += 'message:' + str(source['message']) + "\n" + delimeter + "\n"
-        if 'exceptions' in source:
-            outstring += 'exceptions:' + str(source['exceptions']) + "\n" + delimeter + "\n"
-        if 'TraceId' in source:
-            outstring += 'TraceId:' + str(source['TraceId']) + "\n" + delimeter + "\n"
+            if 'message' in source:
+                outstring += 'message:' + str(source['message']) + "\n" + delimeter + "\n"
+            if 'exceptions' in source:
+                outstring += 'exceptions:' + str(source['exceptions']) + "\n" + delimeter + "\n"
+            if 'TraceId' in source:
+                outstring += 'TraceId:' + str(source['TraceId']) + "\n" + delimeter + "\n"
 
-   # except:
-    #    outstring = 'Server is not resolved'
-
+    except Exception as e:
+        template = "An exception of type {0} occurred. Arguments:\n{1!r}"
+        outstring = template.format(type(e).__name__, e.args)
     try:
         resp = make_response(outstring)
     except UnboundLocalError:
